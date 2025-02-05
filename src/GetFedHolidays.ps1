4e8cf30741502acd0e33253a9cbcf34f74aef8ad
Bruce Jernell <59374529+opconWizard@users.noreply.github.com>
2025-02-05 08:36:00 -0500

    v1.6
    -Removed OpConModule.psm1 requirement, the script is now self contained
    -Fixed issue with Fed website after 2/4/25 update
    -Fixed issue with differently formatted dates in OpCon calendars causing a 500 API error
    -Added compatibility with older PS versions

--- src/GetFedHolidays.ps1
+++ src/GetFedHolidays.ps1
@@ -7,66 +7,90 @@ You can use traditional MSGIN functionality or the OpCon API.  Also added a "deb
 so that you can view the dates that will be added.
 
 Author: Bruce Jernell
-Version: 1.5
+Version: 1.6
 #>
 param(
-    $opconmodule,                                             # Path to OpCon API function module
     $msginPath,                                               # Path to MS LSAM MSGIN directory
     $url,                                                     # OpCon API URL
     $apiUser,                                                 # OpCon API User
     $apiPassword,                                             # OpCon API Password
     $extUser,                                                 # OpCon External Event user
     $extPassword,                                             # OpCon External Event password
-    $extToken,                                                # OpCon External Token (OpCon Release 20+)
+    $extToken,                                                # OpCon External Token (OpCon Release 20+, can be used instead of API User/PW for API option)
     $calendar,                                                # OpCon Calendar (ex "Master Holiday")
-    $option = "debug"                                         # Script option: "api", "msgin", "debug"
+    $option = "debug"                                         # Script option: "api", "msgin", "debug" (displays dates but makes no changes)
 )
 
+#Stop on any error
 $ErrorActionPreference = 'Stop'
-
-#Force TLS 1.2/1.3
-if($PSVersionTable.PSVersion.Major -le 5)
-{
-    Write-Output "The Federal Holiday script for OpCon now only supports Powershell version 6+"
-    Write-Output 'Check your PowerShell version or update your OpCon script runner to something like: pwsh.exe -ExecutionPolicy Bypass -File $FILE $ARGUMENTS'
-    Exit 99
-}
-elseif($PSVersionTable.PSVersion.Major -eq 6 -or ($PSVersionTable.PSVersion.Major -eq 7 -and $PSVersionTable.PSVersion.Minor -lt 1))
-{ 
-    Write-Output "Using TLS 1.2"
-    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-}
-else 
-{ 
-    Write-Output "Using TLS 1.3"
-    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls13 
-}
 
 if($option -eq "api")
 {
-    #Verifies opcon module exists and is imported
-    if(Test-Path $opconmodule)
+    #Skip Self Signed Certificates in PS 5 or lower
+    if($PSVersionTable.PSVersion.Major -le 5)
     {
-        #Import needed module
-        Import-Module -Name $opconmodule -Force #-Verbose  #If you uncomment this option you will see a list of all functions
-
-        #Skip self signed certificates (OpCon API default)
-        OpCon_SkipCerts
-
-        if($extToken)
-        { 
-            if($extToken -like "Token*")
-            { $token = $extToken }
-            else
-            { $token = "Token " + $extToken }
+        try
+        {
+            Add-Type -TypeDefinition  @"
+            using System.Net;
+            using System.Security.Cryptography.X509Certificates;
+            public class TrustAllCertsPolicy : ICertificatePolicy
+            {
+                public bool CheckValidationResult(
+                ServicePoint srvPoint, X509Certificate certificate,
+                WebRequest request, int certificateProblem)
+                {
+                    return true;
+                }
+            }
+"@
+            [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
         }
-        else
-        { $token = "Token " + (OpCon_Login -url $url -user $apiUser -password $apiPassword).id }
+        catch
+        { Write-Output "Error Ignoring Self Signed Certs" }
+    }
+
+    #Set TLS version
+    if($PSVersionTable.PSVersion.Major -eq 7 -and $PSVersionTable.PSVersion.Minor -gt 1) 
+    { 
+        #Force TLS 1.3
+        Write-Output "Using TLS 1.3"
+        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls13 
     }
     else
     {
-        Write-Host "Unable to import OpCon API module!"
-        Exit 100
+        #Force TLS 1.2
+        Write-Output "Using TLS 1.2"
+        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    }
+
+    if($extToken)
+    { 
+        if($extToken -like "Token*")
+        { $token = $extToken }
+        else
+        { $token = "Token " + $extToken }
+    }
+    else
+    { 
+        $body = @{
+            "user"= @{"loginName"=$apiuser;"password"=$apipassword};
+            "tokenType"=@{"type"="User"}
+        }        
+                
+        try
+        {
+            if($PSVersionTable.PSVersion.Major -lt 6)
+            { $token = "Token " + (Invoke-Restmethod -Method POST -Uri ($url + "/api/tokens") -Body ($body | ConvertTo-Json) -ContentType "application/json").id }
+            else
+            { $token = "Token " + (Invoke-Restmethod -Method POST -Uri ($url + "/api/tokens") -Body ($body | ConvertTo-Json) -ContentType "application/json" -SkipCertificateCheck).id }
+        }
+        catch [Exception]
+        {
+            write-output $_
+            write-output $_.Exception.Message
+            Exit 1
+        }    
     }
 }
 elseif($option -eq "msgin")
@@ -94,26 +118,27 @@ elseif($option -ne "debug")
 }
 
 $months = @("January","February","March","April","May","June","July","August","September","October","November","December")
-$holidays = @("New Year*Day","Martin Luther King","Washington*Birthday","Memorial Day","Juneteenth National Independence Day","Independence Day","Labor Day","Columbus Day","Veterans Day","Thanksgiving Day","Christmas Day")
+$holidays = @("New Year's Day","Birthday of Martin Luther King","Washington's Birthday","Memorial Day","Juneteenth National","Independence Day","Labor Day","Columbus Day","Veterans Day","Thanksgiving Day","Christmas Day")
 $result = Invoke-RestMethod -Uri "https://www.federalreserve.gov/aboutthefed/k8.htm"
 $source = $result.Split("`n",[StringSplitOptions]::RemoveEmptyEntries)
 
 # Parses through the HTML source code and extracts the dates
 for($x=0;$x -lt ($source.Count-1);$x++)
 {
-    if($source[$x] -like "*Holidays Observed by the Federal Reserve System*")
+    if($source[$x] -like "*K.8 - Holidays Observed by the Federal Reserve System*")
     {
         $yearstart = $source[$x].Substring(58,4)
         $yearend = $source[$x].Substring(63,4)
     }
-    elseif($holidays | ForEach-Object{ if($source[$x] -like "*" + $_ + "*"){ return $true }})
+    elseif($holidays | ForEach-Object{ if($source[$x] -like ("*" + $_ + "*")){ return $true }})
     {
         $x++
         for($y=0;$y -le ($yearend-$yearstart);$y++)
         {
             If($source[$x] -notlike "*div>*" -and $source[$x] -ne "")
             {
-                $date = $source[$x].Substring(7,$source[$x].IndexOf("</td>")-($source[$x].IndexOf("<td>")+4))
+                $source[$x] = $source[$x].Trim() # Remove starting spaces (normalize in case this is changed)
+                $date = $source[$x].Substring(4,$source[$x].IndexOf("</td>")-($source[$x].IndexOf("<td>")+4)) #Formerly 7
 
                 $year = [int]$yearstart + $y
                 if($date.indexof('>*') -ge 0)   # Formerly ($date -like "*<*")
@@ -157,15 +182,78 @@ for($x=0;$x -lt ($source.Count-1);$x++)
 
                 if($option -ne "debug")
                 {
+                    if(!$calendar)
+                    {
+                        Write-Output "No calendar name specified!"
+                        Exit 103
+                    }
+
                     if($option -eq "api")
-                    {   
-                        OpCon_UpdateCalendar -url $url -token $token -name $calendar -date $date
-                        if($error)
-                        { 
-                            Write-Output $error
-                            Write-Output "There was a problem updating calendar "$calendar" with date "$date 
-                            Exit 5
+                    {
+                        $counter = 0
+
+                        try
+                        {
+                            $counter = 0
+
+                            if($PSVersionTable.PSVersion.Major -lt 6)
+                            { $getcalendar = Invoke-RestMethod -Method GET -Uri ($url + "/api/calendars?name=" + $calendar) -Headers @{"authorization" = $token} -ContentType "application/json" }
+                            else 
+                            { $getcalendar = Invoke-RestMethod -Method GET -Uri ($url + "/api/calendars?name=" + $calendar) -Headers @{"authorization" = $token} -ContentType "application/json" -SkipCertificateCheck }
+
+                            $getcalendar | ForEach-Object{ $counter++ } 
+                            
+                            if($counter -eq 0)
+                            {
+                                Write-Output "No calendars found by supplied name!"
+                                Exit 3
+                            }
                         }
+                        catch [Exception]
+                        {
+                            Write-Output $_
+                            Write-Output $_.Exception.Message
+                            Exit 3
+                        }
+                    
+                        if($date)
+                        {  
+                            for($z=0;$z -lt $getcalendar[0].dates.Count;$z++)
+                            {
+                                $getcalendar[0].dates[$z] = Get-Date -date $getcalendar[0].dates[$z] -Format "M/d/yyyy"
+                            }
+
+                            if($date -notin $getcalendar[0].dates)
+                            { 
+                                $getcalendar[0].dates += $date
+                                $body = @{
+                                    "id" = $getcalendar[0].Id;
+                                    "name" = $calendar;
+                                    "dates" = $getcalendar[0].dates
+                                }
+        
+                                try
+                                { 
+                                    if($PSVersionTable.PSVersion.Major -lt 6)
+                                    { $update = Invoke-RestMethod -Method PUT -Uri ($url + "/api/calendars/" + $getcalendar[0].id) -Body ($body | ConvertTo-JSON -Depth 5) -Headers @{"authorization" = $token} -ContentType "application/json" }
+                                    else 
+                                    { $update = Invoke-RestMethod -Method PUT -Uri ($url + "/api/calendars/" + $getcalendar[0].id) -Body ($body | ConvertTo-JSON -Depth 5) -Headers @{"authorization" = $token} -ContentType "application/json" -SkipCertificateCheck}
+                                }
+                                catch [Exception]
+                                {
+                                    Write-Output $_
+                                    Write-Output $_.Exception.Message
+                                    Exit 2
+                                }
+                            }
+                            else 
+                            { Write-Output "Date $date already in calendar $calendar !" }
+                        }
+                        else
+                        { 
+                            Write-Output "Issue getting date from Federal Reserve site" 
+                            Exit 5
+                        }          
                     }
                     elseif($option -eq "msgin")
                     {
